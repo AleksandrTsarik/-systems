92e312e1f222ee0ae347a7b915b3e164bc6a1391
TsarikAleksandr <ascwork86@gmail.com>
2021-03-01 11:44:55 +0500

    change dialogItem

--- src/components/Dialogs/DialogItem/DialogItem.jsx
+++ src/components/Dialogs/DialogItem/DialogItem.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-// import DialogsAdd from "./DialogsAdd/DialogsAdd";
+
 
 const DialogItem = (props) => {
     let path = "/dialogs/user/" + props.id;
